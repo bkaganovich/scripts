c51c0a96b26a0ba09f6a343e37eddf49c823c50e
bkaganovich <bkgnovich@gmail.com>
2022-11-08 12:47:43 +0200

    v3

--- tar-backup.sh
+++ tar-backup.sh
@@ -1,24 +1,51 @@
 #!/bin/bash
 
 # File: tar-backup.sh
-# Version: 2
+# Version: 3
 # Create gzip tarball archive backup of specified directories and files.
 
-today=$(date +'%Y.%m.%d')
+# Prompts for archive name (i.e. myarchive)
+# Prompts for full path of target input dir (/home/username/Documents)
+# Prompts if to change default backup dir, prompts for new output backup dir
 
+# Default output backup dir (ending with /):
+backupdir="$HOME/backups/"
+
+#timestamp format yyyy.mm.dd.hhmmss
+today=$(date +'%Y.%m.%d.%H%M%S')
 
 #enter archive name
-filename="directory-$today.tar.gz"
+read -p "Enter archive name: " archive
 
-#enter path of target dir
-files="directory/"
+#enter path of target input dirs
+read -p "Enter full path of target input dir: " targetdir
+files="$targetdir/"
 
-cd /media/usb/documents/ #change dir
+out_dir () {
+    printf "Current output directory: $backupdir\n"
+}
 
-tar -czvf /home/benny/backups/$filename $files #make archive at destination
+out_dir #show current output dir
 
-#tar -czvf ./backups/$filename $files #make archive
+#prompt for output backup dir
+read -p "Change output directory? [y/n] " choice
 
+if [ "$choice" == "y" ] || [ "$choice" == "Y" ];
+then
+    read -p "Enter full path for output dir (ending with /): " backupdir
+    out_dir #show current output dir
+fi
+
+#filename formatting
+filename="$archive-$today.tar.gz"
+
+#archive operation
+tar -czvf $backupdir$filename $files #make archive at destination
+
+#result
+printf "\nCreated $backupdir$filename\n"
+
+#timer
 if (( $SECONDS > 3600 )) ; then
     let "hours=SECONDS/3600"
     let "minutes=(SECONDS%3600)/60"
